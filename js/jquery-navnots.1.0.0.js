4b13dc765ebf245a43f08ef39e643fc5e2329203
Tony Monckton <tony@purpletang.co.uk>
2017-09-25 11:30:33 +0100

    First commit

--- js/jquery-navnots.1.0.0.js
+++ js/jquery-navnots.1.0.0.js
@@ -26,35 +26,91 @@
  */
 
 	$.fn.NavDots = function(options) {
-		var $body = $('html, body');
-		var settings = $.extend({
-	    	duration: 500,
-	    	easing: "easeOutQuad"
+		var $body 		= $('html, body');
+		var settings 	= $.extend({
+			align:		"top",
+	    	duration: 	500,
+	    	easing:		"easeOutQuad"
 	  	}, options );
+		
+		this.update = function()
+		{
+			var navTop		= 0;
+			
+			switch (options.align) 
+			{
+				case 'top':	
+					navTop = 0;
+				break;
+				case 'center':
+					var navDotListH	= $('#navDots').height();
+					var winH		= window.innerHeight;
+					navTop 			= (winH/2)-(navDotListH/2);
+				break;
+				case 'bottom':
+					var navDotListH	= $('#navDots').height();
+					var winH		= window.innerHeight;
+					navTop 			= winH-navDotListH;
+				break;
+			}
+
+			$("#navDots").css('padding-top', navTop);
+		}
+
+		this.reSizeMe = function ()
+		{
+			this.update()
+		} 
+
+		this.update()
 	
 		return this.each(function() {
 			$(this).on({
 			    click: function(){
-					//alert("click");
 					if ( $(this).hasClass('dotOff') )
 					{
-						$('li').each(function()	{
+						$('.dotList li').each(function()	{
 							if ($(this).hasClass('dotOn')) { $(this).removeClass('dotOn').addClass('dotOff');} 
 						});
+						
 			        	$(this).addClass('dotOn').removeClass('dotOff');
-			
+						
 						$body.stop().animate({ scrollTop: $($(this).data("href")).offset().top }, settings.duration, settings.easing);
 					}
+					if ( $(this).hasClass('navDown') )
+					{
+						var $cSection = $(".dotList .dotOn");
+						var $nSection = $($cSection).next();
+						if ( $nSection !== undefined )	
+						{
+							if ( $($nSection).hasClass("navDown") == false )
+							{
+								$($cSection).removeClass('dotOn').addClass('dotOff');		
+								$($nSection).removeClass('dotOff').addClass('dotOn'); 
+								$body.stop().animate({ scrollTop: $($($nSection).data("href")).offset().top }, settings.duration, settings.easing);
+							}
+						}
+					}
+					if ( $(this).hasClass('navUp') )
+					{
+						var $cSection = $(".dotList .dotOn");
+						var $nSection = $($cSection).prev();
+						if ( $nSection !== undefined )	
+						{
+							if ( $($nSection).hasClass("navUp") == false )
+							{
+								$($cSection).removeClass('dotOn').addClass('dotOff');				
+								$($nSection).removeClass('dotOff').addClass('dotOn'); 
+								$body.stop().animate({ scrollTop: $($($nSection).data("href")).offset().top }, settings.duration, settings.easing);
+							}
+						}
+					}
 			    },
-			    mousedown: function()	{},		// :active state
-			    mouseup: function() 	{},		// on click release
-			    mouseenter: function()	{		// on hover
-					$(this).css("opacity", '.5');
-			    },
-			    mouseleave: function()	{		// hover exit
-					$(this).css("opacity", '1.0');
-			    }, 
-				hover: function()		{}		// or hover instead of enter/leave
+			    mousedown: function()	{},	// :active state
+			    mouseup: function() 	{},	// on click release
+			    mouseenter: function()	{	$(this).css("opacity", '.5');	},	// on hover
+			    mouseleave: function()	{	$(this).css("opacity", '1.0');	},	// hover exit
+				hover: function()		{}	// or hover instead of enter/leave
 			});		
-	    });  
+	    }); 
 	};
